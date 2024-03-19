1b9423e2fc930fbce859f981d0a09ad1fbc9ab2a
rheakher <rheakher@gmail.com>
2024-03-19 21:40:04 +0000

    Added markers

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -51,7 +51,7 @@ function Home() {
             <CardFooter
                 className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
                 <Button onClick={() => {
-                    window.location.href = "/holi-menu-ar/menu.jpg"
+                    window.location.href = "/holi-menu-ar/menu.pdf"
                 }} className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg"
                         size="sm">
                     View Menu
